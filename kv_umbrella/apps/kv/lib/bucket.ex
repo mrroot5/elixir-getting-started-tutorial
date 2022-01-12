b9790e51a08ad7ba7ba1186943738a11ca28fc21
Adrian Garrido <adrian.garrido@doofinder.com>
2022-01-12 19:38:54 +0100

    Added kv_umbrella umbrella project

---
+++ kv_umbrella/apps/kv/lib/bucket.ex
@@ -0,0 +1,34 @@
+defmodule KV.Bucket do
+  use Agent, restart: :temporary
+
+  @spec start_link((() -> any)) :: {:error, any} | {:ok, pid}
+  def start_link(_opts) do
+    Agent.start_link(fn -> %{} end)
+  end
+
+  @doc """
+  Gets a value from the `bucket` by `key`.
+  """
+  @spec get(pid, Strin.t()) :: %{String.t() => any}
+  def get(bucket, key) do
+    Agent.get(bucket, &Map.get(&1, key))
+  end
+
+  @doc """
+  Puts the `value` for the given `key` in the `bucket`.
+  """
+  @spec put(pid, String.t(), any) :: atom
+  def put(bucket, key, value) do
+    Agent.update(bucket, &Map.put(&1, key, value))
+  end
+
+  @doc """
+  Deletes `key` from `bucket`.
+
+  Returns the current value of `key`, if `key` exists.
+  """
+  @spec delete(pid, String.t()) :: any
+  def delete(bucket, key) do
+    Agent.get_and_update(bucket, &Map.pop(&1, key))
+  end
+end
